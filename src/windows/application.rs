69bf9a5488f5ebdf88a3c69bbc15a69815a99504
Tom Gardiner <tom@tombofry.co.uk>
2023-02-10 12:20:12 +0000

    Move Project and Windows away from Application

--- src/windows/application.rs
+++ src/windows/application.rs
@@ -1,82 +1,8 @@
 use crate::{
-	resources::{assets::setup_custom_fonts, strings::PROJECT_DEFAULT_NAME},
-	windows::{
-		menu::draw_application_menu,
-		mixer::{Channel, Mixer},
-		Window,
-	},
+	data::project::Project,
+	resources::assets::setup_custom_fonts,
+	windows::{menu::draw_application_menu, Windows},
 };
-use egui::{Context, Ui};
-use std::collections::BTreeSet;
-
-pub struct Project {
-	pub name: String,
-}
-
-impl Project {
-	pub fn new(name: &str) -> Self {
-		Self {
-			name: name.to_owned(),
-		}
-	}
-}
-
-pub struct Windows {
-	windows: Vec<Box<dyn Window>>,
-
-	open: BTreeSet<String>,
-}
-
-impl Default for Windows {
-	fn default() -> Self {
-		Self::new(vec![
-			Box::new(Mixer {
-				channels: vec![
-					Channel::new(Some("Master")),
-					Channel::new(Some("Channel 1")),
-					Channel::new(Some("Channel 2")),
-				],
-			}),
-			// Box::new(super::code_editor::CodeEditor::default()),
-		])
-	}
-}
-
-impl Windows {
-	pub fn new(windows: Vec<Box<dyn Window>>) -> Self {
-		let open = BTreeSet::new();
-
-		Self { windows, open }
-	}
-
-	pub fn checkboxes(&mut self, ui: &mut Ui) {
-		let Self { windows, open } = self;
-		for window in windows {
-			let mut is_open = open.contains(window.name());
-			ui.toggle_value(&mut is_open, window.name());
-			Windows::set_open(open, window.name(), is_open);
-		}
-	}
-
-	pub fn windows(&mut self, ctx: &Context) {
-		let Self { windows, open } = self;
-		for window in windows {
-			let mut is_open = open.contains(window.name());
-			window.show(ctx, &mut is_open);
-			Windows::set_open(open, window.name(), is_open);
-		}
-	}
-
-	fn set_open(open: &mut BTreeSet<String>, key: &'static str, is_open: bool) {
-		if is_open {
-			if !open.contains(key) {
-				open.insert(key.to_owned());
-			}
-		} else {
-			open.remove(key);
-		}
-	}
-}
 
 pub struct SystemState {
 	pub project: Project,
@@ -88,7 +14,7 @@ impl SystemState {
 		setup_custom_fonts(&cc.egui_ctx);
 
 		Self {
-			project: Project::new(PROJECT_DEFAULT_NAME),
+			project: Project::default(),
 			windows: Windows::default(),
 		}
 	}
--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -1,3 +1,7 @@
+use self::mixer::Mixer;
+use egui::{Context, Ui};
+use std::collections::BTreeSet;
+
 pub mod application;
 pub mod menu;
 pub mod mixer;
@@ -12,4 +16,51 @@ pub trait Window {
 
 pub trait View {
 	fn ui(&mut self, ui: &mut egui::Ui);
+}
+
+pub struct Windows {
+	windows: Vec<Box<dyn Window>>,
+	open: BTreeSet<String>,
+}
+
+impl Default for Windows {
+	fn default() -> Self {
+		Self::new(vec![Box::new(Mixer::default())])
+	}
+}
+
+impl Windows {
+	pub fn new(windows: Vec<Box<dyn Window>>) -> Self {
+		let open = BTreeSet::new();
+
+		Self { windows, open }
+	}
+
+	pub fn checkboxes(&mut self, ui: &mut Ui) {
+		let Self { windows, open } = self;
+		for window in windows {
+			let mut is_open = open.contains(window.name());
+			ui.toggle_value(&mut is_open, window.name());
+			Windows::set_open(open, window.name(), is_open);
+		}
+	}
+
+	pub fn windows(&mut self, ctx: &Context) {
+		let Self { windows, open } = self;
+		for window in windows {
+			let mut is_open = open.contains(window.name());
+			window.show(ctx, &mut is_open);
+			Windows::set_open(open, window.name(), is_open);
+		}
+	}
+
+	fn set_open(open: &mut BTreeSet<String>, key: &'static str, is_open: bool) {
+		if is_open {
+			if !open.contains(key) {
+				open.insert(key.to_owned());
+			}
+		} else {
+			open.remove(key);
+		}
+	}
 }
