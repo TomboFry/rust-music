eb6add85b8c19d7a0d8e9ba4aa2a4b19551a1597
Tom Gardiner <tom@tombofry.co.uk>
2023-02-10 12:21:35 +0000

    Replace File menu items with "Exit"

--- src/windows/menu.rs
+++ src/windows/menu.rs
@@ -8,23 +8,16 @@ pub fn draw_application_menu(ctx: &Context) {
 	});
 }
 
-// Taken from https://github.com/emilk/egui/blob/master/crates/egui_demo_lib/src/demo/demo_app_windows.rs
-// while I still work things out.
 fn file_menu_button(ui: &mut Ui) {
-	let organize_shortcut =
-		egui::KeyboardShortcut::new(Modifiers::CTRL | Modifiers::SHIFT, egui::Key::O);
-	let reset_shortcut =
-		egui::KeyboardShortcut::new(Modifiers::CTRL | Modifiers::SHIFT, egui::Key::R);
+	let quit_shortcut = egui::KeyboardShortcut::new(Modifiers::CTRL, egui::Key::Q);
+	// let reset_shortcut =
+	// 	egui::KeyboardShortcut::new(Modifiers::CTRL | Modifiers::SHIFT, egui::Key::R);
 
 	// NOTE: we must check the shortcuts OUTSIDE of the actual "File" menu,
 	// or else they would only be checked if the "File" menu was actually open!
 
-	if ui.input_mut(|i| i.consume_shortcut(&organize_shortcut)) {
-		ui.ctx().memory_mut(|mem| mem.reset_areas());
-	}
-
-	if ui.input_mut(|i| i.consume_shortcut(&reset_shortcut)) {
-		ui.ctx().memory_mut(|mem| *mem = Default::default());
+	if ui.input_mut(|i| i.consume_shortcut(&quit_shortcut)) {
+		std::process::exit(0);
 	}
 
 	ui.menu_button("File", |ui| {
@@ -32,26 +25,11 @@ fn file_menu_button(ui: &mut Ui) {
 		ui.style_mut().wrap = Some(false);
 
 		if ui
-			.add(
-				egui::Button::new("Organize Windows")
-					.shortcut_text(ui.ctx().format_shortcut(&organize_shortcut)),
-			)
+			.add(egui::Button::new("Exit").shortcut_text(ui.ctx().format_shortcut(&quit_shortcut)))
 			.clicked()
 		{
-			ui.ctx().memory_mut(|mem| mem.reset_areas());
 			ui.close_menu();
-		}
-
-		if ui
-			.add(
-				egui::Button::new("Reset egui memory")
-					.shortcut_text(ui.ctx().format_shortcut(&reset_shortcut)),
-			)
-			.on_hover_text("Forget scroll, positions, sizes etc")
-			.clicked()
-		{
-			ui.ctx().memory_mut(|mem| *mem = Default::default());
-			ui.close_menu();
+			std::process::exit(0);
 		}
 	});
 }
