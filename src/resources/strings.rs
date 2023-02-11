d5039124503bfd5aa25fd9b229c8ec1bbaf505fc
Tom Gardiner <tom@tombofry.co.uk>
2023-02-11 18:18:19 +0000

    Store window state in a BTreeMap instead of Vec
    This will allow us to more easier get specific window state from within
    SystemState.

--- src/resources/strings.rs
+++ src/resources/strings.rs
@@ -1,3 +1,5 @@
 pub const WINDOW_TITLE: &'static str = "Tombo's DAW";
 pub const PROJECT_DEFAULT_NAME: &'static str = "Untitled Project";
+pub const WINDOWS_LABEL: &'static str = "Windows:";
+
 pub const CHANNEL_DEFAULT_NAME: &'static str = "Channel";
--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -1,6 +1,10 @@
 use self::{mixer::Mixer, sampler::Sampler};
-use egui::{Context, Ui};
-use std::collections::BTreeSet;
+use crate::resources::strings;
+use egui::Context;
+use std::{
+	any::Any,
+	collections::{BTreeMap, BTreeSet},
+};
 
 pub mod application;
 pub mod menu;
@@ -18,45 +22,52 @@ pub trait Window {
 }
 
 pub struct Windows {
-	windows: Vec<Box<dyn Window>>,
+	windows: BTreeMap<&'static str, Box<dyn Window>>,
 	open: BTreeSet<String>,
 }
 
 impl Default for Windows {
 	fn default() -> Self {
-		Self::new(vec![
-			Box::new(Mixer::default()),
-			Box::new(Sampler::default()),
-		])
+		let mut windows: BTreeMap<&'static str, Box<dyn Window>> = BTreeMap::new();
+		windows.insert("Mixer", Box::new(Mixer::default()));
+		windows.insert("Sampler", Box::new(Sampler::default()));
+
+		Self::new(windows)
 	}
 }
 
 impl Windows {
-	pub fn new(windows: Vec<Box<dyn Window>>) -> Self {
-		let open = BTreeSet::new();
+	pub fn new(windows: BTreeMap<&'static str, Box<dyn Window>>) -> Self {
+		let mut open = BTreeSet::new();
+		open.insert("Mixer".to_owned());
 
 		Self { windows, open }
 	}
 
-	pub fn checkboxes(&mut self, ui: &mut Ui) {
-		let Self { windows, open } = self;
-		for window in windows {
-			let mut is_open = open.contains(window.name());
-			ui.toggle_value(&mut is_open, window.name());
-			Windows::set_open(open, window.name(), is_open);
-		}
+	pub fn checkboxes(&mut self, ctx: &Context) {
+		let Self { open, windows } = self;
+		egui::TopBottomPanel::bottom("application-windows").show(ctx, |ui| {
+			ui.horizontal(|ui| {
+				ui.label(strings::WINDOWS_LABEL);
+				for (name, _) in windows {
+					let mut is_open = open.contains(*name);
+					ui.toggle_value(&mut is_open, *name);
+					Windows::set_open(open, name, is_open);
+				}
+			})
+		});
 	}
 
 	pub fn windows(&mut self, ctx: &Context) {
 		let Self { windows, open } = self;
-		for window in windows {
-			let mut is_open = open.contains(window.name());
-			window.show(ctx, &mut is_open);
-			Windows::set_open(open, window.name(), is_open);
+		for (name, window) in windows {
+			let mut is_open = open.contains(*name);
+			window.show(ctx, name, &mut is_open);
+			Windows::set_open(open, name, is_open);
 		}
 	}
 
-	fn set_open(open: &mut BTreeSet<String>, key: &'static str, is_open: bool) {
+	fn set_open(open: &mut BTreeSet<String>, key: &str, is_open: bool) {
 		if is_open {
 			if !open.contains(key) {
 				open.insert(key.to_owned());
